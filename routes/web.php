7f982c025857b9c1e21727bf0f9c1e24f508f553
FarizSatria <farizsatria18@gmail.com>
2024-04-28 00:01:29 +0700

    fortify, login, logout

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => 'dashboard']);
+Route::get('/', function () {
+    return view('pages.auth.auth-login');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+});
+
